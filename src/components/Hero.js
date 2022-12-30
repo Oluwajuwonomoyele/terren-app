e238fa95a49c3f017e4309657ae08096f267d851
Olutomisin Oluwajuwon <jolutomisin@gmail.com>
2022-12-30 02:46:28 +0100

    data update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
             <div className="container mx-auto px-6 md:px-14 py-14 lg:py-28 text-white flex flex-col-reverse lg:flex-row gap-8 lg:justify-between items-center relative z-10">
                <div className='lg:basis-[60%]'>
                     <h1 data-aos='fade-down' className='text-[35px] leading-[2.5rem] xl:leading-[4rem] sm:text-4xl xl:text-[50px] font-bold'>Creating economic alliances among African <span className='text-darkGrey'>companies</span></h1>
-                    <p className='mt-4 xl:mt-8 leading-6 lg:text-lg' data-aos='fade-right'>Terren connects real estate companies and financial institutions, with the financial institutions investing in the real estate companies, thereby easing the generation of short-term capital and benefiting the financial institutions with higher returns on investment deposit.</p>
+                    <p className='mt-4 xl:mt-8 leading-6 lg:text-lg' data-aos='fade-right'>Terren connects real estate companies and financial institutions, we revolutionize investment with our fractional funding strategy that allows multiple financial institutions to flexibly invest in a single real estate project, thereby easing the generation of short-term capital and benefiting the financial institutions with higher returns on investment deposits.</p>
                </div>
                <div className='hidden lg:block lg:basis-1/2 lg:overflow-hidden rounded-md' data-aos='zoom-out'>
                     <img src={HeroBg} alt="Hero bg" />
